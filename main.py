2d8790f9785e583c746ca7a5868d20fb17157d51
indra-inc <indranath.james@gmail.com>
2023-01-15 20:45:40 +0530

    main.py updated

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ warnings.filterwarnings("ignore")
 
 
 ## creating function for test sentence
-@st.cache
+
 def evaluateDisplayNer(inp):
   # Loaded newly created model-best
 
   nlp_custom_ner = spacy.load('model-best')
   test_txt = inp
-  print('')
   user_doc = nlp_custom_ner(test_txt)
 
   return user_doc
@@ -22,18 +21,18 @@ def evaluateDisplayNer(inp):
 
 st.title('Custom NER: Corona')
 st.header('Put your input string : ')
-inp = st.text_input()
-st.write('Your Input String is: ', inp)
+inp = st.text_input(label = "")
+
 doc = evaluateDisplayNer(inp)
 
 colors = {"PATHOGEN": "#F67DE3", "MEDICINE": "#7DF6D9", "MEDICALCONDITION":"#85C1E9"}
 options = {"colors": colors} 
 
-st.subheader('The Result is: ')
+st.subheader("-------- List of Entity and their Label --------")
 for ent in doc.ents:
-  st.write(ent.text, "|", ent.label_, '|', spacy.explain(ent.label_))
+  st.write(ent.text, "|", ent.label_, '|')
 
-print('-------DISPLACY------')
+st.subheader("-------- Entity and Label Display --------")
 ent_html = displacy.render(doc, style = 'ent',options= options, jupyter= False)
 st.markdown(ent_html, unsafe_allow_html=True)
 
